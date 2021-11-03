edc4f99de16645e3f10124bda230f01eef599c58
Andrei Dzebysh <presnia@yahoo.com>
2021-11-03 17:31:23 +0300

    The project is done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
+gsap.to('#img', {
+  scrollTrigger: {
+    scrub: 0.5,
+  },
+  x: 1500,
+  y: 400,
+});
 
+gsap.from('#wolf', {
+  scrollTrigger: {
+    scrub: 0.5,
+  },
+  x: 400,
+});
+
+gsap.from('#castle', {
+  scrollTrigger: {
+    scrub: 0.5,
+  },
+  x: -90,
+});
+
+gsap.to('#bats', {
+  scrollTrigger: {
+    scrub: 0.5,
+  },
+  scale: 1.5,
+});
